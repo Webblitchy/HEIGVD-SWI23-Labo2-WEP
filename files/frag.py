80e85e2ffc3156ede80a16d40c3dd4f805d0ba8e
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2023-04-18 14:11:39 +0200

    add fragments

--- files/frag.py
+++ files/frag.py
@@ -3,27 +3,29 @@
 
 """ Manually decrypt a wep message given the WEP key"""
 
-__author__      = "Abraham Rubinstein"
-__copyright__   = "Copyright 2017, HEIG-VD"
-__license__ 	= "GPL"
-__version__ 	= "1.0"
-__email__ 		= "abraham.rubinstein@heig-vd.ch"
-__status__ 		= "Prototype"
+__author__ = "Abraham Rubinstein"
+__copyright__ = "Copyright 2017, HEIG-VD"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "abraham.rubinstein@heig-vd.ch"
+__status__ = "Prototype"
 
 from scapy.all import *
 import binascii
 from rc4 import RC4
-#Cle wep AA:AA:AA:AA:AA
-key = b'\xaa\xaa\xaa\xaa\xaa'
+
+# Cle wep AA:AA:AA:AA:AA
+key = b"\xaa\xaa\xaa\xaa\xaa"
+
 
 def gen_packet(data):
-    #lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
-    arp = rdpcap('arp.cap')[0]
+    # lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
+    arp = rdpcap("arp.cap")[0]
 
     # Calcul de l'ICV
     icv = binascii.crc32(data)
     # Transformation en bytes de l'ICV
-    icv = icv.to_bytes(4, 'little')
+    icv = icv.to_bytes(4, "little")
 
     # notre plaintext sont les données concaténées avec l'ICV
     ptext = data + icv
@@ -38,13 +40,13 @@ def gen_packet(data):
     # l'ICV est les derniers 4 octets
     icv_encrypted = ctext[-4:]
     # On converti dans le bon format (en long) pour le paquet
-    icv_encrypted = struct.unpack('!L', icv_encrypted)[0]
+    icv_encrypted = struct.unpack("!L", icv_encrypted)[0]
 
     # Les données sont tout le ciphertext sauf les 4 derniers octets
     message_encrypted = ctext[:-4]
 
-    print ('Text: ', message_encrypted)
-    print ('icv:  ', icv_encrypted)
+    print("Text: ", message_encrypted)
+    print("icv:  ", icv_encrypted)
 
     # On met la length à None pour que Scapy recalcul la longueur de lui-même
     arp[RadioTap].len = None
@@ -53,12 +55,16 @@ def gen_packet(data):
     arp.icv = icv_encrypted
     return arp
 
+
 # On enregistre dans le fichier pcap
-wrpcap('fichier.pcap', arp, append=True)
 
 # Définition de notre message en clair
-data = bytes.fromhex('aaaa03000000080600010800060400019027e4ea61f2c0a80164000000000000c0a801c8deadbeef')
+data = bytes.fromhex(
+    "aaaa03000000080600010800060400019027e4ea61f2c0a80164000000000000c0a801c8deadbeef"
+)
 
-p1 = gen_packet(data)
-p2 = gen_packet(data)
-p3 = gen_packet(data)
+normal_packet = gen_packet(data)
+fragments = fragment(normal_packet, fragsize=8)
+
+for arp in fragments:
+    wrpcap("fichier-frag.pcap", arp, append=True)
